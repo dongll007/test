37a2078ff03f2a45e52e70a7108e0e10be4fb3ec
dongll007 <313737043@qq.com>
2019-12-09 11:21:18 +0800

    init gittest project

--- src/main/java/com/banma/gittest/GittestApplication.java
+++ src/main/java/com/banma/gittest/GittestApplication.java
@@ -3,6 +3,7 @@ package com.banma.gittest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class GittestApplication {
 
